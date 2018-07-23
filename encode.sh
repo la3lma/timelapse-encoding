4e186a9d6ddb2142a1e77b54978433fcb3a551e3
Bjørn Remseth <rmz@comoyo.com>
2018-07-23 11:27:24 +0200

    Parametrizing resolution more accurately

--- encode.sh
+++ encode.sh
@@ -8,13 +8,15 @@ END=$(find JPG -name '*.JPG' -print| sed 's|^JPG/DSC_||g' | sed  's|\.JPG$||g' |
 
 echo "start = $START, end = $END"
 
+HIGH_RESOLUTION="6016x4000"
+MEDIUM_RESOLUTION="1280x720"
 
 #Frames per second
 FPS=16
 
 
 # First we make a medium res version
-ffmpeg -r $FPS -start_number $START -i "JPG/DSC_%04d.JPG" -s 1280x720 -vcodec libx264 MPG/out_med_res.mpg
+ffmpeg -r $FPS -start_number $START -i "JPG/DSC_%04d.JPG" -s $MEDIUM_RESOLUTION -vcodec libx264 MPG/out_med_res.mpg
 
 # Then we make a maximum resolution version
-ffmpeg -r $FPS -start_number $START -i "JPG/DSC_%04d.JPG"  -vcodec libx264 MPG/out_high_res.mpg
+ffmpeg -r $FPS -start_number $START -i "JPG/DSC_%04d.JPG"  -s $HIGH_RESOLUTION  -vcodec libx264 MPG/out_high_res.mpg
